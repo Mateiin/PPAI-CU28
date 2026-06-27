9c979c8572dbe45dd4c3f43c4824de345cc1bfcf
Juan Ignacio Llufriu <juanillufriu@gmail.com>
2026-06-27 19:03:36 -0300

    fix: downgrade typeorm to version 0.3.20 and update database synchronization settings
    - Changed typeorm version from ^1.0.0 to ^0.3.20 in package.json.
    - Updated app.module.ts to set synchronize to true and commented out dynamic configuration for database connection.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,36 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RecepcionBolsinModule } from './recepcion-bolsin/recepcion-bolsin.module';
 
+// console.log({
+//       host: process.env.DB_HOST,
+//       user: process.env.DB_USERNAME,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_DATABASE
+//     });
 @Module({
   imports: [
+    
     ConfigModule.forRoot({ isGlobal: true }),
+//     TypeOrmModule.forRootAsync({
+//   imports: [ConfigModule],
+//   inject: [ConfigService],
+//   useFactory: (cfg: ConfigService) => ({
+//     type: 'postgres',
+//     host: cfg.get('DB_HOST'),
+//     port: cfg.get<number>('DB_PORT'),
+//     username: cfg.get('DB_USER'),
+//     password: cfg.get('DB_PASSWORD'),
+//     database: cfg.get('DB_NAME'),
+
+//     synchronize: true,
+
+//     autoLoadEntities: true,
+
+//     logging: true,
+
+//     logger: 'advanced-console',
+//   }),
+// }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -16,7 +43,9 @@ import { RecepcionBolsinModule } from './recepcion-bolsin/recepcion-bolsin.modul
         database: cfg.get('DB_NAME', 'bolsines'),
         username: cfg.get('DB_USER', 'postgres'),
         password: cfg.get('DB_PASSWORD', 'postgres'),
-        synchronize: cfg.get('DB_SYNCHRONIZE') === 'true',
+        // synchronize: cfg.get('DB_SYNCHRONIZE') === 'true',
+        synchronize: true,
+
         autoLoadEntities: true,
       }),
     }),
